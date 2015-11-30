c4617e89f015947019e4cf5833eddc10112dd512
32hiko <nemurenaimachi@gmail.com>
2015-12-01 00:43:23 +0900

    エンジンが持つ盤に、全部の駒を配置

--- src/shogi/ban.go
+++ src/shogi/ban.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 )
 
+// alias
+var p = fmt.Println
+var s = fmt.Sprint
+
 type TBan struct {
 	// 直感的な数字でわかるように10*10とし、0の要素は使わない。
 	Masu [10][10]*TMasu
 	// 駒IDをキーに、駒へのポインタを持つマップ
+	AllKoma   map[byte]*TKoma
 	SenteKoma map[byte]*TKoma
 	GoteKoma  map[byte]*TKoma
 }
@@ -16,20 +21,31 @@ func NewBan() *TBan {
 	masu := [10][10]*TMasu{}
 	// マスを初期化する
 	var x, y byte = 1, 1
-	for x <= 9 {
-		y = 1
-		for y <= 9 {
+	for y <= 9 {
+		x = 1
+		for x <= 9 {
 			masu[x][y] = NewMasu(0)
-			y++
+			x++
 		}
-		x++
+		y++
 	}
 	ban := TBan{
 		Masu:      masu,
+		AllKoma:   make(map[byte]*TKoma),
 		SenteKoma: make(map[byte]*TKoma),
 		GoteKoma:  make(map[byte]*TKoma),
 	}
 	return &ban
+}
+
+func (ban TBan) PutKoma(koma *TKoma) {
+	ban.AllKoma[koma.Id] = koma
+	if koma.Side {
+		ban.SenteKoma[koma.Id] = koma
+	} else {
+		ban.GoteKoma[koma.Id] = koma
+	}
+	ban.Masu[koma.Position[0]][koma.Position[1]].KomaId = koma.Id
 }
 
 type TMasu struct {
@@ -53,21 +69,87 @@ type Relations struct {
 }
 
 func CreateInitialState() *TBan {
-	fmt.Println("CreateInitialState()")
+	p("CreateInitialState()")
 	ban := NewBan()
 	// 駒を1つずつ生成する
 	var koma_id byte = 1
-	// まずは後手の歩9枚だけ
+
+	// 後手
 	var side bool = false
-	var i byte = 1
-	for i <= 9 {
-		pos := [2]byte{i, 3}
-		fu := NewFu(koma_id, pos, side)
-		ban.GoteKoma[koma_id] = fu
-		masu := ban.Masu[i][3]
-		masu.KomaId = koma_id
+	// 香
+	ban.PutKoma(NewKoma(koma_id, Kyo, [2]byte{1, 1}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kyo, [2]byte{9, 1}, side))
+	koma_id++
+	// 桂
+	ban.PutKoma(NewKoma(koma_id, Kei, [2]byte{2, 1}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kei, [2]byte{8, 1}, side))
+	koma_id++
+	// 銀
+	ban.PutKoma(NewKoma(koma_id, Gin, [2]byte{3, 1}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Gin, [2]byte{7, 1}, side))
+	koma_id++
+	// 金
+	ban.PutKoma(NewKoma(koma_id, Kin, [2]byte{4, 1}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kin, [2]byte{6, 1}, side))
+	koma_id++
+	// 王
+	ban.PutKoma(NewKoma(koma_id, Gyoku, [2]byte{5, 1}, side))
+	koma_id++
+	// 角
+	ban.PutKoma(NewKoma(koma_id, Kaku, [2]byte{2, 2}, side))
+	koma_id++
+	// 飛
+	ban.PutKoma(NewKoma(koma_id, Hi, [2]byte{8, 2}, side))
+	koma_id++
+	// 歩
+	var x byte = 1
+	for x <= 9 {
+		ban.PutKoma(NewKoma(koma_id, Fu, [2]byte{x, 3}, side))
 		koma_id++
-		i++
+		x++
+	}
+
+	// 先手
+	side = true
+	// 香
+	ban.PutKoma(NewKoma(koma_id, Kyo, [2]byte{1, 9}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kyo, [2]byte{9, 9}, side))
+	koma_id++
+	// 桂
+	ban.PutKoma(NewKoma(koma_id, Kei, [2]byte{2, 9}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kei, [2]byte{8, 9}, side))
+	koma_id++
+	// 銀
+	ban.PutKoma(NewKoma(koma_id, Gin, [2]byte{3, 9}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Gin, [2]byte{7, 9}, side))
+	koma_id++
+	// 金
+	ban.PutKoma(NewKoma(koma_id, Kin, [2]byte{4, 9}, side))
+	koma_id++
+	ban.PutKoma(NewKoma(koma_id, Kin, [2]byte{6, 9}, side))
+	koma_id++
+	// 王
+	ban.PutKoma(NewKoma(koma_id, Gyoku, [2]byte{5, 9}, side))
+	koma_id++
+	// 角
+	ban.PutKoma(NewKoma(koma_id, Kaku, [2]byte{8, 8}, side))
+	koma_id++
+	// 飛
+	ban.PutKoma(NewKoma(koma_id, Hi, [2]byte{2, 8}, side))
+	koma_id++
+	// 歩
+	x = 1
+	for x <= 9 {
+		ban.PutKoma(NewKoma(koma_id, Fu, [2]byte{x, 7}, side))
+		koma_id++
+		x++
 	}
 
 	// 各マスの関係を更新する
@@ -77,15 +159,19 @@ func CreateInitialState() *TBan {
 func (ban TBan) Display() string {
 	var str string = ""
 	var x, y byte = 1, 1
-	for x <= 9 {
-		y = 1
-		for y <= 9 {
-			if ban.Masu[x][y].KomaId != 0 {
-				str = str + "(" + fmt.Sprint(x) + "," + fmt.Sprint(y) + ")=" + fmt.Sprint(ban.Masu[x][y].KomaId)
+	for y <= 9 {
+		x = 1
+		for x <= 9 {
+			if ban.Masu[x][y].KomaId == 0 {
+				str = str + "[   ]"
+			} else {
+				// str = str + "(" + s(x) + "," + s(y) + ")=" + s(ban.Masu[x][y].KomaId)
+				str = str + "[" + ban.AllKoma[ban.Masu[x][y].KomaId].Display() + "]"
 			}
-			y++
+			x++
 		}
-		x++
+		str = str + "\n"
+		y++
 	}
 	return str
 }
--- src/shogi/koma.go
+++ src/shogi/koma.go
@@ -9,9 +9,11 @@ const (
 	Gin
 	Kin
 	Kaku
-	Hisha
+	Hi
 	Gyoku
 )
+
+var disp_map = map[byte]string{Fu:"歩", Kyo:"香", Kei:"桂", Gin:"銀", Kin:"金", Kaku:"角", Hi:"飛", Gyoku:"玉"}
 
 type TKoma struct {
 	Id       byte
@@ -22,14 +24,24 @@ type TKoma struct {
 	MoveTo   *[][2]byte
 }
 
-func NewFu(id byte, position [2]byte, side bool) *TKoma {
-	fu := TKoma{
+func NewKoma(id byte, kind byte, position [2]byte, side bool) *TKoma {
+	koma := TKoma{
 		Id:       id,
-		Kind:     Fu,
+		Kind:     kind,
 		Position: position,
 		Side:     side,
 		Promoted: false,
 		MoveTo:   nil,
 	}
-	return &fu
+	return &koma
+}
+
+func (koma TKoma) Display() string {
+	var side_str string
+	if koma.Side {
+		side_str = "▲"
+	} else {
+		side_str = "△"
+	}
+	return side_str + disp_map[koma.Kind]
 }
