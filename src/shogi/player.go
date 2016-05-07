1f5e1217f3ffafeb8741152db020307cbcadacc6
32hiko <nemurenaimachi@gmail.com>
2016-05-07 14:11:41 +0900

    局面の評価値をキャッシュするようにした。

--- src/shogi/player.go
+++ src/shogi/player.go
@@ -3,7 +3,6 @@ package shogi
 import (
 	. "logger"
 	"math/rand"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -132,12 +131,14 @@ func GetMaxKikiMove(ban *TBan, all_moves *map[int]*TMove) *TMove {
 
 type TMainPlayer struct {
 	Joseki *TJoseki
+	Cache  map[string]int
 }
 
 func NewMainPlayer() *TMainPlayer {
 	joseki := NewJoseki()
 	player := TMainPlayer{
 		Joseki: joseki,
+		Cache:  make(map[string]int),
 	}
 	return &player
 }
@@ -212,40 +213,45 @@ func (player TMainPlayer) GetJosekiMove(ban *TBan, all_moves *map[int]*TMove) *T
 	return nil
 }
 
-func GetScore(sfen string, teban TTeban, all_moves *map[int]*TMove, is_disp bool) <-chan string {
+func (player TMainPlayer) GetScore(sfen string, teban TTeban, all_moves *map[int]*TMove, is_disp bool) <-chan string {
 	score_channel := make(chan string)
 
 	for key, move := range *all_moves {
 		// ゴルーチンによりマルチコアを使い、時間短縮をはかる
-		go scoreRoutine(sfen, teban, key, move, score_channel, is_disp)
+		go player.scoreRoutine(sfen, teban, key, move, score_channel, is_disp)
 	}
 
 	return score_channel
 }
 
-func scoreRoutine(sfen string, teban TTeban, key int, move *TMove, score_channel chan string, is_disp bool) {
+func (player TMainPlayer) scoreRoutine(sfen string, teban TTeban, key int, move *TMove, score_channel chan string, is_disp bool) {
 	logger := GetLogger()
 	new_ban := FromSFEN(sfen)
 	move_string := move.GetUSIMoveString()
 	// 実際に動かしてみる
 	new_ban.ApplyMove(move_string)
-	result_sente, result_gote := new_ban.Analyze()
-	score := Evaluate(result_sente, result_gote, teban)
-	if move.IsForward(teban) {
-		// 前に進む手を評価する
-		score += 50
-	}
-	if new_ban.IsTadaMove(move) {
-		// score -= 100000
-		// logger.Trace("penalty move: " + move_string)
-	}
-	score /= 50
-	if is_disp {
-		Resp("info time 0 depth 1 nodes 1 score cp "+ToDisplayScore(score, teban)+" pv "+move_string, logger)
-	}
-	resp_string := s(key) + ":" + s(score)
-	if IsOute(new_ban, !teban) {
-		resp_string += ":Oute"
+
+	resp_string := ""
+	new_sfen := new_ban.ToSFEN(false)
+	cached_score, ok := player.Cache[new_sfen]
+	if ok {
+		resp_string = s(key) + ":" + s(cached_score)
+	} else {
+		result_sente, result_gote := new_ban.Analyze()
+		score := Evaluate(result_sente, result_gote, teban)
+		if move.IsForward(teban) {
+			// 前に進む手を評価する
+			score += 50
+		}
+		score /= 50
+		if is_disp {
+			Resp("info time 0 depth 1 nodes 1 score cp "+ToDisplayScore(score, teban)+" pv "+move_string, logger)
+		}
+		player.Cache[new_sfen] = score
+		resp_string = s(key) + ":" + s(score)
+		if IsOute(new_ban, !teban) {
+			resp_string += ":Oute"
+		}
 	}
 	// "key_of_all_moves:score:Oute(if oute)"
 	score_channel <- resp_string
@@ -268,179 +274,11 @@ func NewNodeScore(key int, moves string, score int, resp_score int) *TNodeScore 
 	return &node_score
 }
 
-func (player TMainPlayer) GetMainBestMove4(ban *TBan, all_moves *map[int]*TMove, hurry bool) (*TMove, int) {
-	logger := GetLogger()
-	teban := *(ban.Teban)
-	current_sfen := ban.ToSFEN(false)
-
-	// 現局面から、自分の全候補手を評価
-	score_channel := GetScore(current_sfen, teban, all_moves, true)
-	move_key_score_map := make(map[int]int)
-	// ゴルーチンの結果待ち
-	for i := 0; i < len(*all_moves); i++ {
-		result := <-score_channel
-		// "key_of_all_moves:score:Oute(if oute)"
-		result_arr := strings.Split(result, ":")
-		key, _ := strconv.Atoi(result_arr[0])
-		score, _ := strconv.Atoi(result_arr[1])
-		move_key_score_map[key] = score
-	}
-
-	node_score_map := make(map[int]*TNodeScore)
-	for key, score := range move_key_score_map {
-		next_ban := FromSFEN(current_sfen)
-		move := (*all_moves)[key]
-		move_string := move.GetUSIMoveString()
-		next_ban.ApplyMove(move_string)
-		next_moves := MakeAllMoves(next_ban)
-		if len(next_moves) == 0 {
-			// 相手の手がないなら詰み（一手詰み）
-			return (*all_moves)[key], 99999
-		}
-		next_sfen := next_ban.ToSFEN(false)
-		// 自分の候補手を指した後の局面から、相手の全候補手を評価
-		next_score_channel := GetScore(next_sfen, !teban, &next_moves, false)
-		next_ks_map := make(map[int]int)
-		// ゴルーチンの結果待ち
-		for i := 0; i < len(next_moves); i++ {
-			result := <-next_score_channel
-			// "key_of_all_moves:score:Oute(if oute)"
-			result_arr := strings.Split(result, ":")
-			key, _ := strconv.Atoi(result_arr[0])
-			score, _ := strconv.Atoi(result_arr[1])
-			next_ks_map[key] = score
-		}
-		// next_ks_mapから、最大の評価値の手を取得する
-		resp_score := -99999
-		resp_key := 0
-		for k, s := range next_ks_map {
-			if resp_score < s {
-				resp_score = s
-				resp_key = k
-			}
-		}
-		// TNodeScoreに保存する
-		if resp_key != 0 {
-			next_move := next_moves[resp_key].GetUSIMoveString()
-			moves_string := move_string + " " + next_move
-			ns := NewNodeScore(key, moves_string, score, resp_score)
-			node_score_map[key] = ns
-		}
-	}
-
-	// TNodeScoreのうち、一部をさらに読む
-	if !hurry {
-		logger.Trace("before CloseUp, length: " + s(len(node_score_map)))
-		new_map := CloseUp(current_sfen, teban, &node_score_map)
-		logger.Trace("ok")
-		node_score_map = new_map
-		logger.Trace("after  CloseUp, length: " + s(len(node_score_map)))
-	}
-
-	// TNodeScoreのうち、RespScoreが最小の手を取得し、返す
-	return_score := 99999
-	return_key := 0
-	for key, entry := range node_score_map {
-		Resp("info time 0 depth 1 nodes 1 score cp "+ToDisplayScore(entry.RespScore, teban)+" pv "+entry.Moves, logger)
-		if return_score > entry.RespScore {
-			return_score = entry.RespScore
-			return_key = key
-		} else {
-			if return_score == entry.RespScore {
-				return_org_score := node_score_map[return_key].Score
-				if return_org_score < entry.Score {
-					return_score = entry.RespScore
-					return_key = key
-				}
-			}
-		}
-	}
-	return (*all_moves)[return_key], node_score_map[return_key].Score
-}
-
-func CloseUp(sfen string, teban TTeban, score_map *map[int]*TNodeScore) map[int]*TNodeScore {
-	logger := GetLogger()
-	width_max := 10
-
-	// RespScoreが小さい上位width件に絞り込む
-	var new_map map[int]*TNodeScore = nil
-	if len(*score_map) <= width_max {
-		// 件数がもともと少ないならこの工程は不要
-		new_map = *score_map
-	} else {
-		// 件数が多い場合、新しいmapに詰め替える
-		new_map = make(map[int]*TNodeScore)
-		// ソートのためのスライス
-		slice := make([]int, len(*score_map))
-		for _, v := range *score_map {
-			slice = append(slice, v.RespScore)
-		}
-		sort.Sort(sort.IntSlice(slice))
-		var border_value int = slice[width_max-1]
-		logger.Trace("slice: " + s(slice))
-		logger.Trace("border_value: " + s(border_value))
-		for k, v := range *score_map {
-			if v.RespScore <= border_value {
-				new_map[k] = v
-				logger.Trace("add key, value: " + s(k) + " " + s(v.RespScore))
-			}
-		}
-	}
-	logger.Trace("CloseUp width: " + s(len(new_map)))
-	for k, v := range new_map {
-		ban := FromSFEN(sfen)
-		moves_arr := strings.Split(v.Moves, " ")
-		ban.ApplyMove(moves_arr[0])
-		ban.ApplyMove(moves_arr[1])
-		next_moves := MakeAllMoves(ban)
-		if len(next_moves) == 0 {
-			// 手がないならこちらが詰み
-			new_v := v
-			new_v.RespScore = 99999
-			new_map[k] = new_v
-		}
-		next_sfen := ban.ToSFEN(false)
-		// 相手の候補手を指した後の局面から、自分の全候補手を評価
-		next_score_channel := GetScore(next_sfen, teban, &next_moves, false)
-		next_ks_map := make(map[int]int)
-		// ゴルーチンの結果待ち
-		for i := 0; i < len(next_moves); i++ {
-			result := <-next_score_channel
-			// "key_of_all_moves:score:Oute(if oute)"
-			result_arr := strings.Split(result, ":")
-			key, _ := strconv.Atoi(result_arr[0])
-			score, _ := strconv.Atoi(result_arr[1])
-			next_ks_map[key] = score
-		}
-		// next_ks_mapから、最大の評価値の手を取得する
-		resp_score := -99999
-		resp_key := 0
-		for k, s := range next_ks_map {
-			if resp_score < s {
-				resp_score = s
-				resp_key = k
-			}
-		}
-		// TNodeScoreに保存する
-		if resp_key != 0 {
-			next_move := next_moves[resp_key].GetUSIMoveString()
-			moves_string := v.Moves + " " + next_move
-			new_v := v
-			// 低いの優先なので反転させる
-			new_v.RespScore = -resp_score
-			new_v.Moves = moves_string
-			new_map[k] = new_v
-		}
-	}
-	logger.Trace("CloseUp final width: " + s(len(new_map)))
-	return new_map
-}
-
 func (player TMainPlayer) GetMainBestMove3(ban *TBan, all_moves *map[int]*TMove, width int, depth int, is_disp bool) (*TMove, int) {
 	logger := GetLogger()
 	teban := *(ban.Teban)
 	current_sfen := ban.ToSFEN(false)
-	score_channel := GetScore(current_sfen, teban, all_moves, is_disp)
+	score_channel := player.GetScore(current_sfen, teban, all_moves, is_disp)
 
 	oute_map := make(map[int]int)
 	better_moves_map := make(map[int]int)
