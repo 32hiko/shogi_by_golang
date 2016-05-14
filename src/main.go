0f57b50d682d8365308d44ceee2487c90b9f1a35
32hiko <nemurenaimachi@gmail.com>
2016-05-15 01:00:58 +0900

    対外的な時間管理を導入した。思考ロジックではまだ時間そのもので管理できていない。

--- src/main.go
+++ src/main.go
@@ -15,6 +15,9 @@ const PROGRAM_NAME = "HoneyWaffle"
 const PROGRAM_VERSION = "2.0.0"
 const AUTHOR = "Mitsuhiko Watanabe"
 
+const NW_LAG_MS = 1000
+const SAFETY_MS = 3000
+
 var master *TBan = nil
 var tesuu int = 0
 var logger *Logger = nil
@@ -133,14 +136,36 @@ func handle_go_text(text string) string {
 	} else {
 		ms, _ = strconv.Atoi(wtime)
 	}
-	bestmove, score := player.Search(master, ms)
-	if len(bestmove) < 6 {
-		master.ApplyMove(bestmove, true, true, true)
-		logger.Trace(master.Display())
-		logger.Trace(master.ToSFEN(true))
-	}
-	Resp(("info time 0 depth 1 nodes 1 score cp " + ToDisplayScore(score, teban) + " pv " + bestmove), logger)
-	return "bestmove " + bestmove
+
+	ms -= NW_LAG_MS
+	time_manager := time.NewTimer(time.Millisecond * time.Duration(ms))
+	player_timer := time.NewTimer(time.Millisecond * time.Duration(ms-SAFETY_MS))
+	ch := make(chan string)
+
+	go func() {
+		<-time_manager.C
+		// TODO select better move before time up
+		stop := player_timer.Stop()
+		if stop {
+			ch <- "resign"
+		}
+	}()
+	go func() {
+		bestmove, _ := player.Search(master, ms)
+		if len(bestmove) < 6 {
+			master.ApplyMove(bestmove, true, true, true)
+			logger.Trace(master.Display())
+			logger.Trace(master.ToSFEN(true))
+		}
+		ch <- bestmove
+		<-player_timer.C
+		time_manager.Stop()
+	}()
+
+	res := <-ch
+	player_timer.Stop()
+	time_manager.Stop()
+	return "bestmove " + res
 }
 
 // タイマーの部分だけサンプル的に作ってみた。
@@ -179,118 +204,4 @@ func handle_go_text_sample(text string) string {
 	response = "bestmove " + res
 
 	return response
-}
-
-func old_main() {
-	// 独自のLoggerを使用
-	InitLogger()
-	logger := GetLogger()
-	defer logger.Close()
-
-	var master *TBan
-	var tesuu int = 0
-	player := NewPlayer("Main")
-
-	// 将棋所とのやりとり
-	// TODO:いつでも返答すべきコマンドは常時listenするイメージで。GoRoutineとChannelを使えばよさげ
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		logger.Req(text)
-		switch text {
-		// エンジン登録時は、usiとquitのみ入力される。
-		case "usi":
-			respUSI(logger)
-		case "quit":
-			// TODO 終了前処理
-			os.Exit(0)
-		case "setoption name USI_Ponder value true":
-			// TODO 設定を保存する
-		case "setoption name USI_Hash value 256":
-			// TODO 設定を保存する
-		case "isready":
-			master = CreateInitialState()
-			tesuu = 0
-			Resp("readyok", logger)
-		case "usinewgame":
-			// TODO: モードを切り替えるべきか。
-		case "gameover":
-			// TODO: 対局待ち状態に戻る。
-		default:
-			if s.HasPrefix(text, "position") {
-				logger.Trace(text)
-				split_text := s.Split(text, " ")
-				// 通常の対局
-				// position startpos moves 7g7f 8b7b 2g2f
-				is_sfen := false
-				if split_text[1] == "sfen" {
-					is_sfen = true
-					// 局面編集からの検討だとこのように
-					// position sfen lnsgkgsnl/1r5b1/1pppppppp/p8/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 2g2f
-					sfen_index := s.Index(text, "sfen")
-					moves_index := s.Index(text, "moves")
-					var sfen_str string
-					if moves_index == -1 {
-						sfen_str = text[sfen_index:]
-					} else {
-						sfen_str = text[sfen_index : moves_index-1]
-					}
-					master = FromSFEN(sfen_str)
-				}
-				if is_sfen {
-					// こちらのルートはどうすればいいのか不明。デッドコピーとしておく。
-					for index, value := range split_text {
-						if index < 7 {
-							continue
-						}
-						// 何度も一手ずつ反映する必要はないので、スキップしている。
-						if index-7 < tesuu {
-							continue
-						}
-						logger.Trace("to apply: " + value)
-						master.ApplyMove(value, true, true, true)
-						logger.Trace(master.Display())
-						tesuu++
-					}
-					// resp("info string "+text, logger)
-				} else {
-					for index, value := range split_text {
-						if index < 3 {
-							continue
-						}
-						// 何度も一手ずつ反映する必要はないので、スキップしている。
-						if index-3 < tesuu {
-							continue
-						}
-						logger.Trace("to apply: " + value)
-						master.ApplyMove(value, true, true, true)
-						logger.Trace(master.Display())
-						logger.Trace(master.ToSFEN(true))
-						tesuu++
-					}
-				}
-			} else if s.HasPrefix(text, "go") {
-				split_text := s.Split(text, " ")
-				btime := split_text[2]
-				wtime := split_text[4]
-				teban := *(master.Teban)
-				var ms int = 0
-				if teban {
-					ms, _ = strconv.Atoi(btime)
-				} else {
-					ms, _ = strconv.Atoi(wtime)
-				}
-				bestmove, score := player.Search(master, ms)
-				if len(bestmove) < 6 {
-					master.ApplyMove(bestmove, true, true, true)
-					logger.Trace(master.Display())
-					logger.Trace(master.ToSFEN(true))
-					tesuu++
-				}
-				Resp(("info time 0 depth 1 nodes 1 score cp " + ToDisplayScore(score, teban) + " pv " + bestmove), logger)
-				bestmove_str := "bestmove " + bestmove
-				Resp(bestmove_str, logger)
-			}
-		}
-	}
 }
